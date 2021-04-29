f2a5a20ff14044c1e471103cf90ee03a1154f331
YanParoni <yan.almeida.paroni@gmail.com>
2021-04-29 17:06:57 -0300

    Questão 1

--- src/average.js
+++ src/average.js
@@ -11,7 +11,17 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {};
+const average = (array) => {
+  let accumulator = 0;
+ if (array.length === 0){
+    return ;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if(typeof(array[i])!== 'number'){return ;}
+    accumulator += array[i];
+  }
+  let total = accumulator / array.length;
+  return Math.round(total);
+};
 
 module.exports = average;
