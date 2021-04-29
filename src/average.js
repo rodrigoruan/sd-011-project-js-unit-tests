e705fde66ba5d6e5bd0c7a0dd6f1cea329921f42
Gabriel <gabriel.omercador@hotmail.com>
2021-04-29 16:18:27 -0300

    Exercicio 1

--- src/average.js
+++ src/average.js
@@ -13,6 +13,25 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sumArray = 0;  
+
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sumArray += array[index];
+  }
+
+  let total = Math.round(sumArray / array.length);
+
+  return total;
+};
+
+console.log(average([]));
 
 module.exports = average;
