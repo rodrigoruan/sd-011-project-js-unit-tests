6b0bc290a99b657c113cff868d8806f00d36f79b
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-04-29 13:26:31 -0300

    Adicionado exercicio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (a) => {
+  return { name: a, feedback: () => 'Eita pessoa boa!' };
+};
 
 module.exports = createStudent;
