2ca1ed73f7084cb5fcc3089e8dd4a0992721e6aa
YanParoni <yan.almeida.paroni@gmail.com>
2021-04-29 17:16:13 -0300

    Linter

--- src/average.js
+++ src/average.js
@@ -13,11 +13,13 @@
 */
 const average = (array) => {
   let accumulator = 0;
- if (array.length === 0){
-    return ;
+  if (array.length === 0) {
+    return;
   }
   for (let i = 0; i < array.length; i += 1) {
-    if(typeof(array[i])!== 'number'){return ;}
+    if (typeof array[i] !== 'number') {
+      return;
+    }
     accumulator += array[i];
   }
   let total = accumulator / array.length;
