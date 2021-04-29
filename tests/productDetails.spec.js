3714d5dfbcd5911cf7c71d7eb3ea3e219e0ae1e4
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-29 19:12:56 -0300

    teste evaluator desafio 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,11 +30,16 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof (productDetails()), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof Object.keys(productDetails('Alcool gel', 'Máscara')), 'object'); // TESTE EVALUATOR
+    // assert.strictEqual(typeof (productDetails('Alcool gel', 'Máscara')[0]), 'object');
+    // assert.strictEqual(typeof (productDetails('Alcool gel', 'Máscara')[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
