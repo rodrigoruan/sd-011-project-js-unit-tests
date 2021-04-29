0f425cf7df60da834a2107bd0e85a6354e0837ce
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-04-29 17:27:52 -0300

    Resolucao requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
-    return undefined
+    return undefined;
   }
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
@@ -26,5 +26,4 @@ trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
 
-console.log(vqv('Tunico', 29))
 module.exports = vqv;
