45557ffc883cd8bec638d4cebd8b8705cdc099b6
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-29 20:41:17 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterx = 2; counterx <= 3; counterx += 1) {
+      myArray.push(counterx);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
