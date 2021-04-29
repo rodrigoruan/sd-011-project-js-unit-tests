ce9ea6470c70c3e013840e497b4085c19d90b6de
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-29 15:47:53 -0300

    Lint

--- src/vqv.js
+++ src/vqv.js
@@ -22,7 +22,7 @@ const vqv = (nome, idade) => {
   }
   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,
   \ntrabalho na Trybe e mando muito em programação!
-  \n#VQV!`
+  \n#VQV!`;
 };
 
 module.exports = vqv;
