a2012aeb7f134dd42aaf8b9e0825e5ae060c2068
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-04-29 18:48:46 -0300

    Corrige ESLint no requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,9 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-
-  if ( nome !== undefined &&  idade !== undefined) {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  if (nome !== undefined && idade !== undefined) {
+    return `Oi, meu nome é ${nome}!\n
+    Tenho ${idade} anos,\n
+    trabalho na Trybe e mando muito em programação!\n#VQV!`;
   }
   return undefined;
 };
