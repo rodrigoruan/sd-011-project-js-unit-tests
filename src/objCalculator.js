31ceb7332e5ad8d1d67f6b3b40d8628af9e23af0
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-29 19:24:43 -0300

    Requisito 7 realizado

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,13 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const createCalcs = () => (
+  { add(A, B) { return Math.floor(A + B); },
+  mult(A, B) { return Math.floor(A * B); },
+  div(A, B) { return Math.floor(A / B); },
+  sub(A, B) { return Math.floor(A - B); },
+});
+
+const calculator = createCalcs(); 
 
 module.exports = calculator;
