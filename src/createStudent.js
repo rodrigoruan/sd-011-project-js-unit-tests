dc4c4a34daedf5ac32c595c1520223741536e441
Wilquer82 <wilquer.fig@gmail.com>
2021-04-29 20:46:42 -0300

    Fifth Step

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const feedback = () => 'Eita pessoa boa!';
+  return {
+    name: nome,
+    feedback,
+  };
+};
 
 module.exports = createStudent;
