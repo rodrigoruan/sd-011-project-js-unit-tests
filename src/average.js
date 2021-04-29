e824fbb49d6099d79e1d211f1b83fb7a9192149f
PH Assis <pieit@live.com>
2021-04-29 16:17:40 -0300

    average ready

--- src/average.js
+++ src/average.js
@@ -1,4 +1,6 @@
+const lodash = require('lodash');
 const assert = require('assert');
+
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
@@ -13,33 +15,33 @@ const assert = require('assert');
     - average([1, '2']) // Retorno: undefined;
 */
 
+let checkString = (array) => lodash.every(array, lodash.isNumber);
+let checkEmpty = (array) => lodash.isEmpty(array);
+
 const average = (array) => {
   let newArray = 0;
   let counter = 0;
   let final;
-  if (array.length !== 0) {
+  if (checkString(array) === false || checkEmpty(array) === true) {
+    final = undefined;
+  } else {
     for (let i = 0; i < array.length; i += 1) {
       newArray += array[i];
       counter += 1;
     }
     final = Math.round(newArray / counter);
-  } else {
-    final = 'undefined';
   }
   return final;
 };
 
-let teste1 = [1, 2, 3, 4, 5];
-let expected = 3;
+// let string = [];
+// let output = checkEmpty(string);
 
-let output = average(teste1);
-console.log(output);
+// console.log(output);
 
-assert.strictEqual(average([1, 2, 3, 4, 5]), 3);
-assert.strictEqual(average([]), 'undefined');
+assert.strictEqual(average([1, 2, 3, 4, 5]), 3, 'Not Equal!');
+assert.strictEqual(average([]), undefined, 'Empty needs to come back "undefined" ');
 assert.strictEqual(average([5, 5, 5, 4]), 5, 'Arredondamento para baixo');
 assert.strictEqual(average([5, 5, 5, 1.4]), 4, 'Arredondamento para cima');
-
+assert.strictEqual(average([5, 5, 5, '5', 5]), undefined, 'String error');
 module.exports = average;
-
-// [2.6, 1.4, 5.7, 3.9];
