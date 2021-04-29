abe5fe37301ae62b4bfb9fea219dc05125489785
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-29 15:14:54 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -1,17 +1,16 @@
-/*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
-  Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
-  Comportamento:
-    - average([2, 2]) // Retorno: 2;
-    - average([1, 1]) // Retorno: 1;
-    - average([1, '2']) // Retorno: undefined;
-*/
-
-const average = () => {};
+const average = (array) => {
+  let sumArray = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }  
+      sumArray += array[index];
+  }
+  let consolePrint = Math.round(sumArray / array.length);
+  return consolePrint;
+};
 
 module.exports = average;
