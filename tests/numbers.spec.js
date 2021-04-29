4bc64227df8e21844b2cd9d1e21d319611ae4b1e
gabiazevedo <gabiazevedoms@gmail.com>
2021-04-29 17:39:05 -0300

    Adiciona requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const { notDeepStrictEqual } = require('assert');
 const assert = require('assert');
 const numbers = require('../src/numbers');
 
@@ -18,10 +19,10 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+    
+    assert.deepStrictEqual(true, true);
+    assert.deepStrictEqual(false, false);
+    assert.deepStrictEqual(false, false);
+    assert.deepStrictEqual(false, false);
   });
 });
