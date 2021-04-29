ff505bac5240e03f331181775bc260d005c1591b
Oryange <oryangestrifezze@gmail.com>
2021-04-29 17:32:06 -0300

    Adiciona requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -7,7 +7,7 @@ const numbers = require('../src/numbers');
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
 
-  Parâmetros:
+  Parâmetros: 
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
   Comportamento:
     - numbers([2, 3, 4]); // Retorna: true
@@ -18,10 +18,14 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]),true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]),false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.strictEqual(numbers([1, 'a', 3]),false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.strictEqual(numbers([' ']),false);   
+
   });
 });
