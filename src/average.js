f2ef830612b690954c047be1e8d7879ea10adbad
Ghislaine <glenox79@gmail.com>
2021-04-29 17:44:09 -0300

    Exercicio 01

--- src/average.js
+++ src/average.js
@@ -12,8 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let resultAverage = 0;
+  let roundResult = 0;
+
+  if (array.length === [''] || array.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined
+    }
+    resultAverage += array[i];
+    roundResult = Math.round(resultAverage / array.length);
+  }
+  return roundResult;
 };
 
 module.exports = average;
