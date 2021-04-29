41df1c80fed9aa66719211897f9c2f08643398db
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-04-29 15:34:43 -0300

    Function average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,24 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const verifyType = (array) => {
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return true;
+    }
+  }
+};
+
+const average = (array) => {
+  if (verifyType(array) || array.length === 0) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+  }
+  const total = sum / array.length;
+  return Math.round(total);
+};
 
 module.exports = average;
