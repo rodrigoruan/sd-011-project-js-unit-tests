414deabc5a164884d48de8c9ce5114cd8c75e880
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-29 17:57:34 -0300

    requisito numbers completo

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,10 +18,17 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    const output1 = numbers([1, 2, 3, 4, 5]);
+    assert.deepStrictEqual(output1, true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    const output2 = numbers([1, 2, '3', 4, 5]);
+    assert.deepStrictEqual(output2, false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    const output3 = numbers([1, 'a', 3]);
+    assert.deepStrictEqual(output3, false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    const output4 = numbers([' ']);
+    assert.deepStrictEqual(output4, false);
   });
 });
