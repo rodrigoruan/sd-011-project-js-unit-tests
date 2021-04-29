f1de210139cad85893d22437c0df3125a5ceaa80
murilodospacos <murilopacos@gmail.com>
2021-04-29 17:32:40 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,7 @@
     - average([1, '2']) // Retorno: undefined;
 */
 const average = (array) => {
-  if ( array.length === 0) return undefined;
+  if (array.length === 0) return undefined;
   for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') return undefined;
   }
