169eee838cd85a157a754f971c388443874647c2
Luan Xavier <luantenorio3@gmail.com>
2021-04-29 17:33:21 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -22,9 +22,9 @@ const vqv = (nome, idade) => {
   }
 
   const message = `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!"`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 
   return message;
 };
