989c9a6d157f0682192ae917dbd125cb0887a831
Amandha W. Barok <amandha.barok@gmail.com>
2021-04-29 11:48:57 -0700

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  let numberOfElements = array.length;
+  let sumNumber = 0
+  for (let i = 0; i < array.length; i += 1) {
+    if(typeof array[i] !== "number") return undefined;
+    let currentNumber = array[i];
+    sumNumber = sumNumber + currentNumber;
+  }
+  return Math.round(sumNumber / numberOfElements);
 };
+
 
 module.exports = average;
