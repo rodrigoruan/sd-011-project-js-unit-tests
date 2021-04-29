f732eb23de25b90d62c724f6476146f557e865d7
Oryange <oryangestrifezze@gmail.com>
2021-04-29 16:27:15 -0300

    Adiciona Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  let size = array.length;
+
+  if (size === 0) return undefined; 
+
+  for (let index = 0; index < size; index += 1) {
+    if (typeof array[index] !== 'number') return undefined;
+    sum += array[index]; 
+  }
+
+  const result = Math.round(sum / size);
+  return result;
+};
 
 module.exports = average;
