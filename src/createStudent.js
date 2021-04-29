a39841a2de1c18b056cd391f3c11ef5f52049b2a
Amandha W. Barok <amandha.barok@gmail.com>
2021-04-29 14:05:36 -0700

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  return {
+    name: name,
+    feedback: () => 'Eita pessoa boa!'
+  }
+};
+console.log(createStudent('Amandha'));
 
 module.exports = createStudent;
