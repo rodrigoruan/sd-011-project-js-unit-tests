32cf9d538053099fde846436a479e3ee7995191d
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-29 16:40:52 -0300

    nao e possivel...

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,8 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (estudante) => {
+function createStudent(estudante) {
   return { name: estudante, feedback: () => 'Eita pessoa boa!' };
-};
+}
 
 module.exports = createStudent;
