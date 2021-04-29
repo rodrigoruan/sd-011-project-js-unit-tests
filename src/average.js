5d91b13263924caeec0d29bef74230313cb4ea83
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-29 15:53:13 -0300

    Correções LINT

--- src/average.js
+++ src/average.js
@@ -23,7 +23,7 @@ const average = (array) => {
   }
   
   let sum = 0;
-  for (let index = 0; index < array.length; index += 1){
+  for (let index = 0; index < array.length; index += 1) {
     sum += array[index];
   }
   const media = sum / array.length;
