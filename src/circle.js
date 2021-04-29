c243418cf8114fd03ed0445ec8d91df2b82d1ac9
marsleite <marsleite@gmail.com>
2021-04-29 18:35:07 -0300

    desafio 5 - :rocket: ufa

--- src/circle.js
+++ src/circle.js
@@ -23,9 +23,14 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
+    area: Number((PI * radius * radius).toFixed(2)),
+    circumference: Number((2 * PI * radius).toFixed(2)),
   };
 };
+
+console.log(Object.keys(circle(1)).length);
+console.log(circle(2).circumference);
+console.log(circle(3).area);
+console.log(circle(3));
 
 module.exports = circle;
