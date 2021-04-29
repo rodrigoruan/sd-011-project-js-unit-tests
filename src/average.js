73fa42fb75798ed000637bb7072967e199b23fa7
Uriel Bochi <urielbochi@gmail.com>
2021-04-29 16:04:16 -0300

    Corrigindo lint 2.0

--- src/average.js
+++ src/average.js
@@ -14,14 +14,14 @@
 
 const average = (array) => {
   let soma = 0;
-  let media = 0
+  let media = 0;
   for (let index = 0; index < array.length; index += 1) {
     soma += array[index];
   }
-  Math.round(media = soma / array.length)
+  Math.round(media = soma / array.length);
   return media;
-}
+};
 
-console.log(average([10, 5]))
+console.log(average([10, 5]));
 
 module.exports = average;
