377021d5da3c0be68516fc1ef6ca80478ce85a9e
BeatrizR <biarl.ramalhorl@gmail.com>
2021-04-29 20:06:39 -0300

    req 01

--- src/average.js
+++ src/average.js
@@ -13,7 +13,15 @@
 */
 
 const average = () => {
-  
-}
+  if (array.length === 0) return undefined;
+  let numberElements = array.length;
+  let resultNumbers = 0;
+  for (let index = 0; index < array,length; index += 1) {
+    if (typeof array[index] !== 'number') return undefined;
+    let numberCurrent = array[index];
+    resultNumbers += numberCurrent;
+  }
+  return Math.round(resultNumbers / numberElements);
+};
 
 module.exports = average;
