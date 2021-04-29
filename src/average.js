5c4c990349022af940df721cd0f2a719c0ece851
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-29 15:37:24 -0300

    Refatora desafio 1

--- src/average.js
+++ src/average.js
@@ -19,7 +19,7 @@ const average = (array) => {
   let numbers = 0;
   
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof(array[index]) !== 'number') {
+    if (typeof (array[index]) !== 'number') {
       return undefined;
     }
     numbers += array[index];
