3b05003c08ab7d4e376d939dcff6e2e91385ad78
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-04-29 16:17:56 -0300

    Exercicio 1 finalizado

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let sum = 0;
+  for (let index = 0; index < arr.length; index += 1) {
+    sum += arr[index]
+  };
+  
+  if (typeof(sum) !== typeof(1)) { return undefined }
+
+  let avg = Math.round(sum / arr.length);
+
+  if(isNaN(avg)) { return undefined }
+
+  return avg;
+};
 
 module.exports = average;
