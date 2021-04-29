4b775c0788db640a602577893738b8d551731e85
Julio Filizzola <juliofilizzola@hotmail.com>
2021-04-29 15:53:52 -0300

    Desafio 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let numberArry = 0;
+  if (array.length === 0) return undefined;
+ 
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') return undefined;
+    numberArry += array[index];
+  }
+
+  return Math.round(numberArry / array.length);
+};
 
 module.exports = average;
