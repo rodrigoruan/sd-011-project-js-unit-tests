7cdcf3a0804733d0679d0156a60e3b001de2d2a7
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-29 16:01:37 -0300

    avarage correction

--- src/average.js
+++ src/average.js
@@ -15,14 +15,34 @@
 const average = (array) => {
   let avarageNumber = 0;
 
+  if (array.length === 0) {
+    return undefined;
+  }
+
   for (const number of array) {
     avarageNumber += number;
-    if (typeof number !== 'number' || array === []) {
+    if (typeof number !== 'number') {
       return undefined;
     }
   }
   
-  return Number.parseInt(avarageNumber / array.length, 10);
+  return Math.round(avarageNumber / array.length, 10);
 };
 
 module.exports = average;
+
+    // const assert = require('assert');
+
+    // assert.strictEqual(average([3, 4, 5]), 4);
+    // assert.strictEqual(average([1, 2, 3, '4', 5]), undefined);
+    // assert.strictEqual(average([0, 0, 0, 0, 0, 0, 0]), 0);
+    // assert.strictEqual(average([1, 2, '3']), undefined);
+    // assert.strictEqual(average([1, 2, 3]), 2);
+    // assert.strictEqual(average([0, 0, 0, 0, 0, 0, 1]), 0);
+    // assert.strictEqual(average([]), undefined);
+    // assert.strictEqual(average([' ']), undefined);
+    // assert.strictEqual(average(['um', 'dois', 'tres']), undefined);
+    // assert.strictEqual(average([47, 63, 122]), 77);
+
+    // assert.strictEqual(average([-11, 2, 5]), -1);
+    // assert.strictEqual(average([-11, -5, 2]), -5);
