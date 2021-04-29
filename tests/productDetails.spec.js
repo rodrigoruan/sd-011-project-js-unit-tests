24db92d2bff9649eded99d644ccc5fec35562b63
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-04-29 14:52:39 -0300

    Adicionado exercicio 4

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,17 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    const f = productDetails('Alcool gel', 'Máscara');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails() === 'object', true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(f.length === 2, true);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof f[0] === 'object' && typeof f[1] === 'object', true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(f[0] === f[1], false);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(/123$/.test(f[0].details.productId) && /123$/.test(f[1].details.productId), true);
   });
 });
