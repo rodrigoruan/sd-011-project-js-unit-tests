dedf8815ae90f4344860a23e757047a783f9e841
IsleneGomes <islenegomes@gmail.com>
2021-04-29 18:12:05 -0300

    Requisito 1..Parte 5

--- src/average.js
+++ src/average.js
@@ -19,7 +19,7 @@ let soma = 0;
   }
 
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof (array[index]) !== 'number') {
+    if (typeof (array[index]) === 'string') {
     return undefined;
   }
       soma += array[index];
