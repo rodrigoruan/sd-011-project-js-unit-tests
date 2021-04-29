1bccf78c0630bc6710fb5c111e370662c4fac50d
murilodospacos <murilopacos@gmail.com>
2021-04-29 17:30:39 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -11,9 +11,12 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {
-  
+const average = (array) => {
+  if ( array.length === 0) return undefined;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') return undefined;
+  }
+  return Math.round(array.reduce((t, n) => n + t) / array.length);
 };
 
 module.exports = average;
