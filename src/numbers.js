12d2c4e66fcc49075887a90df0463838cac36ff1
Gustavo Mendes <gusm217@gmail.com>
2021-04-29 20:47:20 -0300

    Adiciona o 2

--- src/numbers.js
+++ src/numbers.js
@@ -11,11 +11,13 @@
 
 const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
-    if (typeof myArray[i] !== 'number') {
+    if (typeof myArray[i] !== "number") {
       return false;
     }
   }
   return true;
 };
+
+console.log(numbers([]));
 
 module.exports = numbers;
