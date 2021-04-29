65f2fd7b74ca29463219a063fd7c6b0c64c1b8a6
LeoFuna <leo.funa@gmail.com>
2021-04-29 18:55:30 -0300

    Requisito 6

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -50,10 +50,12 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
     assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
+
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
+    
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
