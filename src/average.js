df4bfd68cffa70a9f4e48395d228ac099c45382d
Laura Gusmao <laurargus@gmail.com>
2021-04-29 15:14:27 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -11,9 +11,33 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const isValid = (arr) => {
+  let result = true;
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof (arr[index]) !== 'number') {
+      result = false;
+    }
+  }
+  return result;
+};
 
-const average = () => {
-   // add your implementation here
+const notEmpty = (arr) => {
+  let result = true;
+  if (!arr.length) {
+    result = false;
+  }
+  return result;
+};
+
+const average = (arr) => {
+  let sum = 0;
+  if (!isValid(arr) || !notEmpty(arr)) {
+    return undefined;
+  }
+  for (let index = 0; index < arr.length; index += 1) {
+    sum += arr[index];
+  }
+  return Math.round(sum / arr.length);
 };
 
 module.exports = average;
