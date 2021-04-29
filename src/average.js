e2b868beca9a8f39a690cebf94e1589630546503
Marco Galindo <marco1408@gmail.com>
2021-04-29 15:31:31 -0300

    requisito 3 finalizado e 1 corrigido com undefined

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (array) => {
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] !== 'number' || array === []) {
+    if (typeof array[index] !== 'number' || array === undefined) {
       return undefined;
     }
     sum += array[index];
