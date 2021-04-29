f3b661f868e1f0cfe6e97d34a4734b7ff52e5e60
Leandro Lopes <leandro_carlos@uol.com.br>
2021-04-29 16:32:00 -0300

    item 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let result;
+  if (typeof nome === 'string') {
+    result = `Oi, meu nome é ${nome}!`;
+    if (typeof idade === number) {
+      result = `Tenho ${idade} anos,
+      trabalho na Trybe e mando muito em programação!
+      #VQV!`;
+    }
+  }
+  return result;
+};
 
 module.exports = vqv;
