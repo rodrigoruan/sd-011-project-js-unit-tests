9f50db9212b26dc4782f1cd2a277e69087b6a886
Maran <maranmorimoto@gmail.com>
2021-04-29 17:14:25 -0300

    Request 1 done

--- src/average.js
+++ src/average.js
@@ -10,9 +10,22 @@
     - average([2, 2]) // Retorno: 2;
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
-    
 */
 
-const average = () => {};
+const average = (array) => {;
+  let summation = 0;
+  if (array.length === 0) {
+    return undefined;
+  };
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== "number") {
+      return undefined
+    }
+    summation += array[index];
+  }
+  return Math.round(summation / (array.length));
+}
+
+console.log(average([ ]));
 
 module.exports = average;
