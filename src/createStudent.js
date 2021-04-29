ae054a843f9a261934742ffe033d5f3e6474989b
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-29 19:22:45 -0300

    Requisitos 5 e 6

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (parameter) => {
+  let estudante = {
+    name: parameter,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
