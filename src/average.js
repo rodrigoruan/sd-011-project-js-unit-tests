0944ea2be2e72145c91019b1097367c664b6c372
guinhoanjos <iago.anjos@hotmail.com>
2021-04-29 19:02:18 -0300

    funcao average feita

--- src/average.js
+++ src/average.js
@@ -12,8 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let media = 0;
+  for (let key of array) {
+    if (typeof(key) !== 'number' || array.length === 0) {
+      return undefined;
+    } else {
+      media += Math.round(key);
+    }
+  }
+  return media/array.length
 };
 
 module.exports = average;
