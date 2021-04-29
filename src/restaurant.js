a771b89599f8f716822760e2ea7a7daa1b1b36ba
Gustavo Lemes <gustavolemes997@gmail.com>
2021-04-29 18:17:03 -0300

    Removendo linha inutil

--- src/restaurant.js
+++ src/restaurant.js
@@ -78,8 +78,6 @@
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`,
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso,
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
-const assert = require('assert');
-
 const totalConsumption = (arr) => {
   let total = 0;
   for (let index = 0; index < arr.length; index += 1) {
