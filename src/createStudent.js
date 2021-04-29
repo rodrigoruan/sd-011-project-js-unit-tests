387104264ab04f127ccaecee515d59d27ec2558b
benlopes <rubenssouss@hotmail.com>
2021-04-29 17:12:40 -0300

    Add createStudent function

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const goodPerson = 'Eita pessoa boa!';
+  const student = {
+    name,
+    feedback: () => goodPerson,
+  };
+  return student;
+};
 
 module.exports = createStudent;
