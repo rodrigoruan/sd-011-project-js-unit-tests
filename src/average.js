991cb9511630ff7fc204f04cd1414440905988a6
fernando lasmar <f3rnandolasmar@gmail.com>
2021-04-29 19:23:53 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-  // adicione sua implementação aqui
+const average = (array) => {
+  let sum = 0;
+  let media = 0;
   
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  media = Math.round(sum / array.length);
+  return media;
+};
+
+console.log(average([3, 4]));
 module.exports = average;
