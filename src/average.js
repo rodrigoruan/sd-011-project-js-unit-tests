01027e6fd16372582d2236f30c5fa0f616343dd4
Marco Galindo <marco1408@gmail.com>
2021-04-29 15:00:02 -0300

    requisito 1 finalizado

--- src/average.js
+++ src/average.js
@@ -12,8 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  //add function here
+const average = (array) => {
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  return sum / array.length;
 };
 
 module.exports = average;
