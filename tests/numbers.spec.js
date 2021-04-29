5a265144016d97cbaa3334e0c09ee0c815e6370b
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-29 17:07:17 -0300

    requisito 2 numbers.js finalizado e passando nos tests

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,10 +18,25 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
+    // assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     // Escreva um teste em que a função recebe [' '] e retorna false
+
+    const param = [1, 2, 3, 4, 5];
+    const funcTest1 = numbers(param);
+    assert.strictEqual(funcTest1, true);
+    const param2 = [1, 2, '3', 4, 5];
+    const funcTest2 = numbers(param2);
+    assert.strictEqual(funcTest2, false);
+    const param3 = [1, 'a', 3];
+    const funcTest3 = numbers(param3);
+    assert.strictEqual(funcTest3, false);
+    const param4 = [' '];
+    const funcTest4 = numbers(param4);
+    assert.strictEqual(funcTest4, false);
+
+
   });
 });
