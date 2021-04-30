d6284dae466c16f0854b7bcc62e92383c3bcb7d0
IsleneGomes <islenegomes@gmail.com>
2021-04-30 18:58:12 -0300

    Requisito 7..de novo

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -21,10 +21,10 @@
 
 const calculator = (num1, num2) => {
   const operacoes = {
-  add: num1 + num2,
-  mult: num1 * num2,
-  div: num1 / num2,
-  sub: num1 - num2,
+  add: Math.floor(num1 + num2),
+  mult: Math.floor(num1 * num2),
+  div: Math.floor(num1 / num2),
+  sub: Math.floor(num1 - num2),
   };
   return operacoes;
 };
