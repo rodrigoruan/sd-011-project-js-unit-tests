c362c17457d8014801f70fdefe215f0683496224
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-30 05:14:54 -0300

    Terceiro Requisito concluído

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (studentName) => {
+  const estudante = {
+    name: studentName,
+    feedback: feedback(),
+  }
+  return estudante;
+};
 
 module.exports = createStudent;
