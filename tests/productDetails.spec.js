3e7e93b78fa43e080a1b143042c9ed3ed1c31cca
Vitor <vitorguima@gmail.com>
2021-04-29 21:07:09 -0300

    exercicio 6 finalizado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,48 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length === 2, true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length === 2, true);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const testArray = (array) => {
+      for (let object of array) {
+        if(Object.keys(object).length === 0) {
+          return false;
+        } return true;
+      } 
+    }
+
+    assert.strictEqual(testArray(productDetails('Alcool gel', 'Máscara')), true);
+
     // Teste que os dois objetos são diferentes entre si.
+    const checkEquals = (array) => {
+      for (let index = 0; index < array.length; index += 1) {
+        if (array[index] === array[index + 1]) {
+          return true;
+        } return false;
+      }
+    }
+
+    assert.deepStrictEqual(checkEquals(productDetails('Alcool gel', 'Máscara')), false);
+
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const verifyId = (array) => {
+  
+      for (let index = 0; index < array.length; index += 1) {
+        let getId = array[index].details.productId;
+        let idToArray = getId.split('');
+    
+        if (idToArray[idToArray.length - 1] === '3' &&
+          idToArray[idToArray.length - 2] === '2' &&
+          idToArray[idToArray.length - 3] === '3') {
+          result = true;
+        } return true;
+      }
+    }
+
+    assert.strictEqual(verifyId(productDetails('Alcool gel', 'Máscara')), true);
   });
 });
