9e41793a1839a0dc40e7d61b8a712f35b04abc6c
IsleneGomes <islenegomes@gmail.com>
2021-04-29 22:52:17 -0300

    Requisito 3..parte 3

--- src/vqv.js
+++ src/vqv.js
@@ -21,9 +21,9 @@ const vqv = (nome, idade) => {
     return undefined;
   }
 
-  return `Oi, meu nome é ${nome}!\n
-          Tenho ${idade} anos,\n
-          trabalho na Trybe e mando muito em programação!\n
+  return `Oi, meu nome é ${nome}!\n +
+          Tenho ${idade} anos,\n +
+          trabalho na Trybe e mando muito em programação!\n +
           #VQV!`;
 };
 
