4d9156c28edffc48812d1ad30889e2bbfb464ad3
Vinicius <viniciusfod@gmail.com>
2021-04-30 20:43:21 -0300

    adiciona requisito 2

--- src/circle.js
+++ src/circle.js
@@ -1,4 +1,4 @@
-/* eslint-disable max-len */
+//* eslint-disable max-len */
 
 /*
   Essa função recebe o raio de um círculo,
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,20 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 entradas.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(typeof circle(10), 'object'); // Teste se circle retorna um objeto.
+    assert.strictEqual(Object.keys(circle(10)).length, 3)// Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(circle(), undefined);// Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(Math.round(circle(2).circumference * 100) / 100, 12.56);// Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(Math.round(circle(3).area * 100) / 100, 28.26);// Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    const resultCircle = circle(3);// Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    resultCircle.circumference = Math.round(resultCircle.circumference * 100) / 100;
+    resultCircle.area = Math.round(resultCircle.area * 100) / 100;
+
+    assert.deepStrictEqual(resultCircle, {
+      radius: 3,
+      circumference: 18.84,
+      area: 28.26,
+    });
   });
 });
