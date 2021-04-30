548644e6baf02dcc2657049a57f62fb9782f1dc6
Oryange <oryangestrifezze@gmail.com>
2021-04-30 11:13:06 -0300

    Adiciona Desafio 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,28 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
+     // ESCREVA SEUS TESTES ABAIXO:
+    const produtos = productDetails('param1', 'param2') //array is array**
+
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(produtos), true);
+    
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(produtos.length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof produtos[0], 'object');
+    assert.deepStrictEqual(typeof produtos[1], 'object');
+   
     // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.notDeepStrictEqual(produtos[0], produtos[1]);
+    
+    // Teste que os dois productIds terminam com 123.//método para verificar final de string
+    const produtos1 = produtos[0].details.productId;
+    assert.strictEqual(produtos1.slice(-3), '123');
+
+    const produtos2 = produtos[1].details.productId;
+    assert.strictEqual(produtos2.slice(-3), '123');
+
   });
 });
