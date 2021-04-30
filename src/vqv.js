bc2ad5b41787ae6748cca19eee0192f38405d645
Oryange <oryangestrifezze@gmail.com>
2021-04-30 12:49:47 -0300

    Adiciona undefined no requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,6 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
 const vqv = (nome, idade) => {
   if (!nome || !idade) return undefined;
 return `Oi, meu nome é ${nome}!
