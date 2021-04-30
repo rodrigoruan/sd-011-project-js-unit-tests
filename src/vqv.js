ff75ddf2c46d8d0d9ccff98b01d1451a54519253
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-04-29 22:01:17 -0300

    Corrige requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome !== undefined && idade !== undefined) {
-    return `Oi, meu nome é ${nome}!\n
-    Tenho ${idade} anos,\n
-    trabalho na Trybe e mando muito em programação!\n#VQV!`;
+  if (nome === undefined || idade === undefined) {
+    return undefined;
   }
-  return undefined;
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
