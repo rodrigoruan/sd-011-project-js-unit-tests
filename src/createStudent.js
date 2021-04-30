b5ee45d59eb6e752425dd2eead1b6238665d8fe0
DADA <davimarque2000@gmail.com>
2021-04-30 14:40:35 -0300

    conclusao do circle (requisitoc 4), e implementaçao da funçao createstudant(requisito 5)

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const feedback = () => 'Eita pessoa boa!';
+  let obj = { name: nome };
+  obj.feedback = feedback;
+  return obj;
+};
 module.exports = createStudent;
