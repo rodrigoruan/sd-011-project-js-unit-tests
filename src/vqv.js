ace64075dbf0221c52c190838ffc8fdd1953784d
Ana Carolina R Santos <kroline0693@gmail.com>
2021-04-29 21:35:05 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -15,14 +15,14 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
-const vqv = (nome, idade) => { 
-console.log(
-(`'Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => {
+  if (!nome || !idade) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`),
-);
+#VQV!`;
 };
 
 module.exports = vqv;
