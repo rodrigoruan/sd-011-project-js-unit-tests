22a059a43e23f51a582bb4e7aada7e69f5e67d3b
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-30 15:39:15 -0300

    Desafio 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,15 +27,22 @@ const productDetails = require('../src/productDetails');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-
+let checkFirst = productDetails('Alcool')[0].details.productId.slice(-3);
+let checkSecond = productDetails('Máscara')[1].details.productId.slice(-3);
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(typeof productDetails(1), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(Object.keys(productDetails(1)).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Alcool gel'), productDetails('Máscara'));
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(checkFirst, '123');
+    assert.strictEqual(checkSecond, '123');
   });
 });
