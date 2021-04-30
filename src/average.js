3e4635476c85584b72a43eea73b5348f9d48f3e5
Vinicius <viniciusfod@gmail.com>
2021-04-30 01:54:21 -0300

    tenta lint

--- src/average.js
+++ src/average.js
@@ -13,17 +13,16 @@
 */
 
 const average = (arrayNumbers) => {
-  let sum = 0
-  if (arrayNumbers.length === 0) {
+let sum = 0;
+if (arrayNumbers.length === 0) {
+  return undefined;
+}
+for (let index of arrayNumbers) {
+  if (typeof index !== 'number') {
     return undefined;
-  }
-  for (let index of arrayNumbers) {
-    if (typeof index !== 'number') {
-      return undefined;
-    }
-    else
-    sum += index;
-  }
+  }else
+  sum += index;
+}
   return Math.round(sum / arrayNumbers.length);
 }
 
