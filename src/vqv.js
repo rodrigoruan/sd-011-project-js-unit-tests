67f517de9a4da61b115ec448a6ad1424bda08575
IsleneGomes <islenegomes@gmail.com>
2021-04-30 14:58:13 -0300

    Requisito 3..de novo e de novo

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-  let paragrafo = (
-  `Oi, meu nome é ${nome}!
+  let paragrafo = `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!`
-  );
+  #VQV!`;
   return paragrafo;
 };
 
