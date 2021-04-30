441506c776db0704d4303cda42421478f7734210
DADA <davimarque2000@gmail.com>
2021-04-30 15:23:44 -0300

    requisito 6, product details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,19 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+  
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('macarrãozin', 'batata')), true)
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('macarrãozin', 'batata').length, 2)
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productDetails('macarrãozin', 'batata')[0]) && typeof (productDetails('macarrãozin', 'batata')[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('macarrãozin', 'batata')[0], productDetails('macarrãozin', 'batata')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const obj1Id = productDetails('macarrãozin', 'batata')[0].details.productId;
+    const obj2Id = productDetails('macarrãozin', 'batata')[1].details.productId;
+    assert.strictEqual(obj1Id.slice(-3) && obj2Id.slice(-3), '123')
   });
 });
