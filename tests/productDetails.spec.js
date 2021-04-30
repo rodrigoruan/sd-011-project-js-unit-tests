6bc8547eeb38fe809a6592adde9c0bc569a7eac7
Gustavo <gusm217@gmail.com>
2021-04-30 13:58:08 -0300

    Começao o 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,5 +1,6 @@
-const assert = require('assert');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const { type } = require("os");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -28,13 +29,16 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Verifica se a função `productDetails` tem o comportamento esperado", () => {
+    // https://www.w3schools.com/jsref/jsref_isarray.asp
+    assert.strictEqual(Array.isArray(productDetails("a", "b")), true);
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Object.keys(productDetails("a", "b")).length, 2);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(typeof Object.keys(productDetails("a", "b")), "object");
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.notStrictEqual(productDetails("a", "b"), "a" !== "b");
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
