a5d7927bb17289b067e6c4d011ae31fcbd9528da
Savio Moraes <smmsavio@hotmail.com>
2021-04-30 01:40:04 -0300

    atualizando codigo

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,6 +31,7 @@ const productDetails = require('../src/productDetails');
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
     assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
     // Teste que os dois itens dentro do array retornado pela função são objetos.
