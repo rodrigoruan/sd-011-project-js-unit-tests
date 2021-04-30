c49a1ba53ca1a4a1f958edfe7ee7e73db7267a82
Vinicius <viniciusfod@gmail.com>
2021-04-30 17:29:26 -0300

    aaaaaaaaaaaaaaa

--- src/average.js
+++ src/average.js
@@ -21,11 +21,10 @@ if (arrayNumbers.length === 0) {
 for (let index of arrayNumbers) {
   if (typeof index !== 'number') {
     return undefined;
-  } else {
+  }
   sum += index;
 }
   return Math.round(sum / arrayNumbers.length);
-}
 }
 
 module.exports = average;
