78c6679737b9b234d2c14bd73f215a95cd448593
DADA <davimarque2000@gmail.com>
2021-04-30 12:54:13 -0300

    resolve B.O do requisit 3 ja vamo pro 4

--- src/average.js
+++ src/average.js
@@ -14,9 +14,7 @@
 
 const average = (array) => {
   let soma = 0;
-  if (array.length === 0) {
-    return undefined;
-  }
+  if (array.length === 0) { return undefined; }
   for (let index = 0; index < array.length; index += 1) {
     if (typeof (array[index]) !== 'number') {
       return undefined;
