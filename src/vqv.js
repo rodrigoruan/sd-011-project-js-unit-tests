4939b120677bb0ebb95d2b1bab9cde5ab1b32a84
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-05-01 10:57:58 -0300

    Adiciona requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (!nome || !idade) return undefined;
+  
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+
+console.log(vqv('Gabi', 33));
 
 module.exports = vqv;
