1b4023ea16517508c289078e345d4934391da851
BeatrizR <biarl.ramalhorl@gmail.com>
2021-05-01 20:47:35 -0300

    agora vai

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -4,19 +4,6 @@
 const assert = require('assert');
 const circle = require('../src/circle');
 
-/*
-  Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
-  Se não for especificado um raio, a função retorna undefined.
-  Elabore testes para verificar se a função está correta.
-
-  Parâmetros:
-    - Um número inteiro. Exemplos: 1; 3;
- 
-  DICA: Números de ponto flutuante em JavaScript são imprecisos!  Para testar, vá no seu navegador e faça `0.2 + 0.1`.
-        Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
-        Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
 
