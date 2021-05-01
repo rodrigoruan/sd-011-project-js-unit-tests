5a2c8455c777ed042e9df826ce73d7f81fc602fd
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-05-01 17:46:13 -0300

    Adiciona requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,17 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
+    // Teste que o retorno da função é um array. Utitilizei o deepStrict entendendo que o teste será realizado em toda função, verificando tipo, valor e childs.
+    assert.deepStrictEqual(Array.isArray(productDetails('first', 'second')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails('first', 'second').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('first', 'second'), 'object');
     // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.notDeepStrictEqual(productDetails('first', 'second'), 'Not Equal');
+    // (Difícil) Teste que os dois productIds terminam com 123. https://www.w3schools.com/jsref/jsref_endswith.asp
+    assert.strictEqual(productDetails('first', 'second')[0].details.productId.endsWith('123') &&
+    productDetails('fist', 'second')[1].details.productId.endsWith('123'), true);
   });
 });
