9c08ffb88dc63103c5b16b63680e09cb001e841d
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-02 18:30:43 -0300

    [add] circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -36,10 +36,10 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.strictEqual(circle(), undefined);
 
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.deepStrictEqual(circle(2).circumference, 12.56);
+    assert.deepStrictEqual(Math.round(circle(2).circumference * 100) / 100, 12.56);
 
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).radius, 3);
+    assert.strictEqual(Math.round(circle(3).radius * 100) / 100, 3);
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
