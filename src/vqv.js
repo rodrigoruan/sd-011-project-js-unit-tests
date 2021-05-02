fc63d5512270d428a0cc4af565d7b1790e1a9cc5
Luiz Fernando Rodrigues <nandovbr15@hotmail.com>
2021-05-02 12:01:08 -0300

    requisito vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+/* Acompanhei a ajuda que alguns colegas tiveram de outros colegas nas salas de estudos */
+const vqv = (nome, idade) => {
+  if (!nome && !idade) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
