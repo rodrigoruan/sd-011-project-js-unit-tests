c72f2f96087ff5624c5dd2597529759cab419d39
nick <pereiranicolas713@gmail.com>
2021-05-02 17:01:33 -0300

    implementacao da function avarege

--- src/average.js
+++ src/average.js
@@ -3,6 +3,7 @@
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+  //
 
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
@@ -12,6 +13,13 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  for (let number of array) {
+    if (typeof number !== 'number') return undefined;
+  }
+  const output = array.reduce((a, b) => a + b) / array.length;
+  return Math.round(output); 
+};
 
 module.exports = average;
