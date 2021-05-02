ee30d5f684decaa751f73c477d27771bfe0ef504
Peter Leite <peterleite.dev@gmail.com>
2021-05-02 13:02:47 -0300

    REQUISITO 3

--- src/vqv.js
+++ src/vqv.js
@@ -18,12 +18,13 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) return undefined;
-  
+
   return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
-console.log(vqv('Tunico', 30));
 
 module.exports = vqv;
+
+console.log(vqv('Tunico', 29));
