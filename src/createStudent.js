30d2fec62ad33ad8b5ab7235e0386333d80f80d8
Peter Leite <peterleite.dev@gmail.com>
2021-05-02 14:55:37 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (studentName) => {
+  const newStudent = {
+    name: studentName,
+    feedback: () => 'Eita, pessoa boa!',
+  };
+  return newStudent;
+};
+const estudante = createStudent('Leandrão, o Lobo Solitário');
+  console.log(estudante.name);
+  console.log(estudante.feedback());
 
 module.exports = createStudent;
