76d5f945a328350f60ff7d86e8ff25bea081429a
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-02 18:21:54 -0300

    [add] circle

--- src/circle.js
+++ src/circle.js
@@ -24,7 +24,7 @@ const circle = (radius) => {
   return {
     radius,
     //! onde aprendi a arredontar https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
-    area: Math.round(PI * radius * radius).toFixed(2),
+    area: Math.round((PI * radius * radius) * 100) / 100,
     circumference: Math.round((2 * PI * radius) * 100) / 100,
   };
 };
