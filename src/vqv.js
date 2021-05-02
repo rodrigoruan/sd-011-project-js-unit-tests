a4b761ac6fe5f3e2d442e233168a0c2cb395cdc0
Lucas Santos <lucassantos_dev@outlook.com>
2021-05-02 07:59:44 -0300

    req 3 passed

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,19 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (idade === undefined) {
+    return undefined; 
+  }
+  if (nome === null) {
+    return undefined;
+  }
+  let text = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return text;
+};
 
+console.log(vqv('Lucas', 26));
 module.exports = vqv;
