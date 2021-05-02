b1852d7c1bda4fc6305eaa3a67b87b7faa5f605b
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-02 02:47:37 -0300

    Realizado exercicio objCalculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,18 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
 
+const calculator = {
+  add: (param1, param2) => param1 + param2,
+  mult: (param1, param2) => param1 * param2,
+  div: (param1, param2) => Math.floor(param1 / param2),
+  sub: (param1, param2) => param1 - param2,
+};
+
+// const assert = require('assert');
+
+// assert.strictEqual(calculator.add(1, 3), 4);
+// assert.strictEqual(calculator.mult(10, 3), 30);
+// assert.strictEqual(calculator.div(5, 2), 2);
+// assert.strictEqual(calculator.sub(1, 3), -2);
 module.exports = calculator;
