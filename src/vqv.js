0c6eaf63c611c0e3b649b70f997f37b37a972b8f
BeatrizR <biarl.ramalhorl@gmail.com>
2021-05-01 21:33:17 -0300

    ajustando erros do req 03

--- src/vqv.js
+++ src/vqv.js
@@ -19,9 +19,9 @@
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) return undefined;
 
-  return `Oi meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
-trabalho na trybe e mando muito em programação!
+trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
 
