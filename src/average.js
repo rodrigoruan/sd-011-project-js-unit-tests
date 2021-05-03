c0da9420ff8df6803334be918c0ea610c8088f29
JoaoCabrera <joaocabrerati@gmail.com>
2021-05-03 11:23:31 -0300

    Adiciona requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  if (array.length === 0) {
+    return undefined;
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
