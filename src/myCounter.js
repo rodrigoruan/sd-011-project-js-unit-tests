594b4e8c4eda4f810b25ab3ac22f254297ae54c0
Vinicius <viniciusfod@gmail.com>
2021-05-03 11:41:01 -0300

    refatora funcao "myCounter"

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  let myArray = [];
+  for (let i = 0; i <= 3; i += 1) {
+    myArray.push(i);
+    for (let a = 2; a <= 3; a += 1) {
+      myArray.push(a);
     }
   }
   return myArray;
