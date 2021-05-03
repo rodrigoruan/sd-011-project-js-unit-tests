103f01a378352b59cc596f080be130c0c1e8b243
gab-boff <gmeninboff@gmail.com>
2021-05-03 00:20:44 -0300

    Adicionando Desafio 6

--- src/productDetails.js
+++ src/productDetails.js
@@ -38,5 +38,5 @@ const productDetails = (firstProduct, secondProduct) => [
     },
   },
 ];
-
+console.log(productDetails('uma string', 'outra string'));
 module.exports = productDetails;
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     // Teste que o array retornado pela função contém dois itens dentro.
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(typeof productDetails(), 'object');
+    assert.strictEqual(productDetails().length, 2);
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[0] === 'object' && typeof productDetails('Alcool gel', 'Máscara')[1], 'object');
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara')[0] !== productDetails('Alcool gel', 'Máscara')[1], true);
+    const endString1 = productDetails()[0].details.productId.split(1);
+    const endString2 = productDetails()[1].details.productId.split(1);
+    assert.deepStrictEqual(endString1[1] === endString2[1], true)
   });
 });
