f52cdb4ad060db719d05242f1c2e0feb1f3cf5a9
Genivaldo <cgenivaldo46@gmail.com>
2021-05-03 04:54:27 -0300

    sem paciencia

--- src/vqv.js
+++ src/vqv.js
@@ -16,13 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => (
-  (nome !== undefined || idade !== undefined)
-  ? `Oi, meu nome é ${nome}!
-  Tenho${idade} anos,
-  trabalho na trybe e mando muito em programação!
-  #VQV`
-  : undefined
-);
-
+const vqv = (nome, idade) => {
+  if (!nome && !idade) {
+    return undefined;
+  } 
+return `'Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!'`;
+};
 module.exports = vqv;
