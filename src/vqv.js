488da33c609945f5234dcfcea180b49134ef2ac7
Genivaldo <cgenivaldo46@gmail.com>
2021-05-03 05:22:57 -0300

    asd

--- src/vqv.js
+++ src/vqv.js
@@ -20,9 +20,9 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   } 
-return `Oi, meu nome é ${nome}! \n`
-+ `Tenho ${idade} anos, \n`
-+ 'trabalho na Trybe e mando muito em programação! \n'
+return `Oi, meu nome é ${nome}!\n`
++ `Tenho ${idade} anos,\n`
++ 'trabalho na Trybe e mando muito em programação!\n'
 + '#VQV!';
 };
 module.exports = vqv;
