c3e1be6654f00cd592b80df1d3c66d82f929a04b
Taís Carvalho <taispcarvalho@gmail.com>
2021-05-03 17:34:49 -0300

    ue

--- src/average.js
+++ src/average.js
@@ -16,6 +16,8 @@ const average = (array) => {
   let sumNumbers = 0;
   let media = sumNumbers / array.length;
 
+  if (array.length === 0) return undefined;
+
   for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') return undefined;
     sumNumbers += array[i];
