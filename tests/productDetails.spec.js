1ed7b697756c26dc33534e1433e19ab854352b27
heyynat <natalifplima@gmail.com>
2021-05-04 12:08:24 -0400

    Correçao na chamada de metodo (requisito 6)

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -4,7 +4,7 @@ const productDetails = require('../src/productDetails');
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // Recebi ajuda de Oryange Strifezze, para solucionar o teste da linha 7
-    assert.deepStrictEqual(Array.isArray.productDetails(), true);
+    assert.deepStrictEqual(Array.isArray(productDetails()), true);
     assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     assert.notDeepStrictEqual(productDetails('alcool','mascara')[0], productDetails('alcool', 'mascara')[1]);
