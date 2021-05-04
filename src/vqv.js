9324a5c68f63e9bec955d850a9049a9b3a60347d
Bernardo Coelho <bernardocezario21@gmail.com>
2021-05-04 14:26:39 -0300

    Complete exercise 9

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const finalText = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return finalText;
+};
 
 module.exports = vqv;
