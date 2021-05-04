8b24e11fac977747330d36a5afac55ebbee2b3a9
Bernardo Coelho <bernardocezario21@gmail.com>
2021-05-04 13:26:22 -0300

    New test for exercise 2

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,7 +29,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.strictEqual(Object.keys(circle(7)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
-    assert.strictEqual(circle(3).area, 28.26);
-    assert.deepStrictEqual(Object.values(circle(3)),[3, 28.26, 18.84]);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(Object.values(circle(3)),[3, 28.259999999999998, 18.84]);
   });
 });
