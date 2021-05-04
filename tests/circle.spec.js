4eb92c5529965afeb53f31d0a1d0c33f6b651502
Anajulia Brito <juliehindius@gmail.com>
2021-05-04 14:41:54 -0300

    Tentando corrigir um erro

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,12 +24,12 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto conteúdos os valores esperados', () => {
-    assert.strictEqual(typeof circle(1), 'Object');
-    assert.strictEqual(object.keys(circle(1)).length, 3);
-    assert.strictEqual(circle(), undefined);
-    assert.strictEqual(circle(2).circumference, 12.56);
-    assert.strictEqual(circle(3).area, 28.259999999999998);
-    assert.strictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
+  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
+    assert.deepStrictEqual(typeof circle (1), 'object');
+    assert.deepStrictEqual(Object.keys(circle(1)).length, 3 );
+    assert.deepStrictEqual(circle(), undefined);
+    assert.deepStrictEqual(circle(2).circumference, 12.56);
+    assert.deepStrictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84});
   });
 });
