60265f6e5cbd7aed3bf69a172eecbdb1d8dfc3de
Genivaldo <cgenivaldo46@gmail.com>
2021-05-04 05:35:52 -0300

    Requesito 06- duvido ir de primeira

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof(productDetails('Alcool gel', 'Mascara')), 'Object')
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('alcool gel', 'Mascara').length, 2)
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(productDetails('Alcool gel', 'Mascara')), 'Object')
+    
     // Teste que os dois objetos são diferentes entre si.
+    let produto = productDetails('Alcool gel', 'Mascara')
+    assert.notStrictEqual(produto[0], produto[1])
+    
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(produto[0].details.productId.endsWith('123'), true)
+    assert.strictEqual(produto[1].details.productId.endsWith('123'), true)
   });
 });
