9849431437b7e2249cfc24b28a358bbba494a6a2
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-05-05 19:44:36 -0300

    vqv fix 6

--- src/vqv.js
+++ src/vqv.js
@@ -4,6 +4,7 @@
   Use template literals para escrever uma função que,
   recebe seu nome e sua idade e retorna o parágrafo descrito abaixo.
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
+  
   Parâmetros:
     - Uma string;
     - Um número.
