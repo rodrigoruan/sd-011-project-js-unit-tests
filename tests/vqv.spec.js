d1ab72c349a326cc1cbf065eade5a3b3c0ab4678
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-05-05 19:39:48 -0300

    vqv fix 4

--- tests/vqv.spec.js
+++ tests/vqv.spec.js
@@ -4,7 +4,6 @@ const vqv = require('../src/vqv.js');
 /*
   Use template literals para escrever uma função que recebe seu nome e sua idade e retorna o parágrafo descrito abaixo.
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
-
   Parâmetros:
     - Uma string;
     - Um número.
@@ -14,7 +13,6 @@ const vqv = require('../src/vqv.js');
       Tenho 30 anos,
       trabalho na Trybe e mando muito em programação!
       #VQV!'
-
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
