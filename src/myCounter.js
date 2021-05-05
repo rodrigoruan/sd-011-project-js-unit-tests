8db685a7161f072185f4ec1ea290f55f897795dd
Genivaldo <cgenivaldo46@gmail.com>
2021-05-05 08:08:12 -0300

    Requesito 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counte = 2; counte <= 3; counte += 1) {
+      myArray.push(counte);
     }
   }
   return myArray;
