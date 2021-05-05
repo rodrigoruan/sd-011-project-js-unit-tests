d17946a1c1f0568d38eec1b994fb5f540c35e284
Víctor Faria <vitilevi@hotmail.com>
2021-05-05 12:04:59 -0300

    Bicudão na cara do lint

--- src/restaurant.js
+++ src/restaurant.js
@@ -3,21 +3,21 @@ function orderFromMenu(str) {
 }
 
 function calculateTotal() {
-  let result = 0;
-  const priceFood = Object.entries(this.fetchMenu().food);
-  const priceDrinks = Object.entries(this.fetchMenu().drink);
-  this.consumption.forEach(order => {
-    priceFood.forEach(food => order === food[0]? result += food[1] : null);
-    priceDrinks.forEach(drink => order === drink[0]? result += drink[1] : null);
-  })
-  return Math.round(result * 1.1);
+  let res = 0;
+  const costF = Object.entries(this.fetchMenu().food);
+  const costD = Object.entries(this.fetchMenu().drink);
+  this.consumption.forEach((order) => {
+    costF.forEach((food) => { const resFood = (order === food[0]) ? res += food[1] : null; });
+    costD.forEach((drink) => { const resDrink = (order === drink[0]) ? res += drink[1] : null; });
+  });
+  return Math.round(res * 1.1);
 }
 
 const createMenu = (objetoRetornado) => ({
   fetchMenu: () => objetoRetornado,
   consumption: [],
   order: orderFromMenu,
-  pay: calculateTotal
+  pay: calculateTotal,
 });
 
 module.exports = createMenu;
