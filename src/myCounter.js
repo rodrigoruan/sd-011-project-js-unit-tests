3084654299ac3e8c3f44338b887ed0fa1897f99b
Rafael-oliveira <rafa_batista02@hotmail.com>
2021-05-05 15:17:02 -0300

    requisito4

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,10 +17,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  for (var count = 0; count <= 3; count += 1) {
+    myArray.push(count);
+    for (var cont = 2; cont <= 3; cont += 1) {
+      myArray.push(cont);
     }
   }
   return myArray;
