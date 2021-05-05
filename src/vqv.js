c51f85c44c9ccfba39d0161c9c447d0d743d8f85
Felipe Carvalho <fecarvalho066@gmail.com>
2021-05-05 15:52:08 -0300

    [Felipe Carvalho] - Requisito 3.2

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,10 @@ const vqv = (nome, idade) => {
   if (nome === null && idade === null) {
     return undefined;
   }
-return `Oi, meu nome é ${nome}!
+  return `'Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`;
+#VQV!'`;
 };
 
 module.exports = vqv;
