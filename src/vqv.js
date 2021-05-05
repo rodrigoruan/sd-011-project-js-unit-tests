ff15280c1be5b70be6403260202e09fbbbd1b192
Rafael-oliveira <rafa_batista02@hotmail.com>
2021-05-05 16:08:51 -0300

    correçao 20

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,15 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome !== 'string' || typeof idade !== 'number') return undefined;
-  return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
-  };
+  if (typeof (nome) === 'undefined' && typeof (idade) === 'undefined') {
+    return undefined;
+  }
+
+  let frase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return frase;
+};
 module.exports = vqv;
