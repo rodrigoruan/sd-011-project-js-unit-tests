ca6c13faf012d095d151c6858e4739d204fb3fbb
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-05 14:28:52 -0300

    Correçoes linter.

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,17 +1,18 @@
 const createMenu = (menu) => {
-  const restaurant = {};
-  restaurant.fetchMenu = () => menu;
-  restaurant.consumption = [];
-  restaurant.order = (orderInput) => restaurant.consumption.push(orderInput);
-  restaurant.pay = () => {
+  const restaurant = {
+  fetchMenu: () => menu,
+  consumption: [],
+  order: (orderInput) => restaurant.consumption.push(orderInput),
+  pay: () => {
     let total = 0;
     restaurant.consumption.forEach((element) => {
       let priceItem = menu.food[element] ? menu.food[element] : menu.drink[element];
       total += priceItem;
     });
     return Math.round(total * 1.10);
-  };
-  return restaurant;
+  }
+}
+return restaurant;
 };
 
 module.exports = createMenu;
