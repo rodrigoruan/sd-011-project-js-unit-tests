d77e49671f41639f1be0448ecc6d5b7313335c24
Antonio Malato <malatoneto@gmail.com>
2021-05-05 15:15:00 -0300

    Correcao lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,15 +16,14 @@
 */
 
 const myCounter = () => {
-  const myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  let myArray = [];
+  for (let index = 0; index <= 3; index += 1) {
+    myArray.push(index);
     for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
-console.log(myCounter())
 
 module.exports = myCounter;
