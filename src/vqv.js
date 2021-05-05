e79fbf48740846a30acdef919b1b255fc21ef4b2
Kevin Willian <kevinwillian084@gmail.com>
2021-05-05 11:41:15 -0300

    Arruma retorno requisito  3

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,7 @@
 */
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
-    return ('Pfv Informe algo');
+    return undefined;
   }
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
