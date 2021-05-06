19a05def95c0b9bf00323107e41823b27fe44681
Nalu <aluziapantoja@gmail.com>
2021-05-06 09:49:41 -0300

    socorro

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,7 +79,6 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
 function checkKey(value, key) {
   for (let index = 0; index < key.length; index += 1) {
     if (value === key[0][index]) {
