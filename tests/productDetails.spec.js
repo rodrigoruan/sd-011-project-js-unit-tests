3fc478e432671a0876e543213e49cb2e01a61568
Luana Souza <luanah.souzza@hotmail.com>
2021-05-06 14:33:50 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,6 +1,3 @@
-/* eslint-disable max-len*/
-/* eslint-disable no-unused-vars */
-
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
@@ -31,20 +28,14 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#productDetails', () => {
-  it('tests the function has the correct behaviour', () => {
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    assert.fail();
+    // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
-    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof productDetails()[0] && typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
-    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-    assert.strictEqual(Object.keys(productDetails()[0]) !== Object.keys(productDetails()[1]), true);
     // (Difícil) Teste que os dois productIds terminam com 123.
-    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
-    assert.strictEqual((productDetails()[0].details.productId).endsWith('123') && (productDetails()[1].details.productId).endsWith('123'), true);
   });
 });
