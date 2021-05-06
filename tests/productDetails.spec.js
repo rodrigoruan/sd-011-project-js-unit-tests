a9bad936526251125b41487a5b2a3d43cde95600
Dario Junior <dario.junior.rs@gmail.com>
2021-05-06 12:49:27 -0300

    resolve a productdetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,41 +1,22 @@
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
-/*
-  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
+function verifyReturn(returnOfFunction) {
+  for (const obj of returnOfFunction) {
+    if (typeof (obj) !== 'object') {
+      return false;
     }
-  ]
-
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
+  }
+  return true;
+}
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(typeof (productDetails('Alcool gel', 'Máscara')), 'object');
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara')[0].name !== productDetails('Alcool gel', 'Máscara')[1].name, true);
+    assert.deepStrictEqual(
+      productDetails('Alcool gel', 'Máscara')[0].details.productId.includes('123')
+      && productDetails('Alcool gel', 'Máscara')[1].details.productId.includes('123'), true);
   });
 });
