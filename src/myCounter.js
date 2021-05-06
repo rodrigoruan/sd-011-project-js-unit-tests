c96b08aaba2c53c71568fc5c8c2761d3b8bcc6ae
IgorMendez <igor.emp@hotmail.com>
2021-05-06 06:31:47 -0300

    .

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,13 +14,12 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
-
+let myArray = [];
 const myCounter = () => {
-  var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var count = 2; count <= 3; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
