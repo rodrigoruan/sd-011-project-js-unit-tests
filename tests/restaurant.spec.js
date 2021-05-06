998d627b29bcbed82388e291309d2797a13b1518
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-06 15:10:31 -0300

    Requisito 9 - item 7

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,3 +1,4 @@
+const { AssertionError } = require('assert');
 const assert = require('assert');
 const { create } = require('eslint/lib/rules/*');
 const createMenu = require('../src/restaurant');
@@ -87,10 +88,12 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // TESTE 5: Verifique que chamar uma função associada à chave `order` no objeto retornado, passando uma string como parâmetro, 
     // como `objetoRetornado.order('coxinha')`, tal string é adicionada ao array retornado em `objetoRetornado.consumption
     // ```
-    // const objetoRetornado = createMenu(objetoQualquer);
-    // objetoRetornado.order("coxinha");
-    // objetoRetornado.consumption // Retorno: ["coxinha"]
+    objetoRetornado = createMenu(objetoQualquer);
+    objetoRetornado.order("coxinha");
+    objetoRetornado.consumption // Retorno: ["coxinha"]
     // ```
+    assert.deepStrictEqual(objetoRetornado.consumption, ["coxinha"]);
+
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens no array `objetoRetornado.consumption` conforme os itens pedidos.
