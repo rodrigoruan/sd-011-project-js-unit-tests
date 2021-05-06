f0852c68298cc15d45c742c3b83f39df7c19fbc1
Aline Hofner <alinehofner@gmail.com>
2021-05-06 20:34:15 -0300

    Average function

--- src/average.js
+++ src/average.js
@@ -12,6 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  if ((array === null) || !array.length) return undefined;
+ for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    } soma += array[index]; 
+  } return Math.round(soma / array.length);
+};
+
   // add your implementation here
 module.exports = average;
