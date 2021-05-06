76074bed8d75d10f8f379e081b56649daad7ced5
Dario Junior <dario.junior.rs@gmail.com>
2021-05-06 17:14:02 -0300

    vamos testar esqueci do lint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,7 +31,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2
     assert.deepStrictEqual(circle(2).circumference, 12.56)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(3).area, 28.26)
+    assert.deepStrictEqual(parseFloat(circle(3).area), 28.26)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3).circumference, 18.84)
   });
