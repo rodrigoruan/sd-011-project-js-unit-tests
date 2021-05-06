25c16156445847305dd24aed8d0c6947011ad57d
heyynat <natalifplima@gmail.com>
2021-05-06 09:25:25 -0400

    Ajuste no requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -6,7 +6,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Recebi ajuda de Oryange Strifezze, para solucionar o teste da linha 7
     assert.deepStrictEqual(Array.isArray(productDetails()), true);
     assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
-    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
+    assert.deepStrictEqual((typeof productDetails('Alcool gel', 'Máscara')[0]) && (typeof productDetails('Alcool gel', 'Máscara')[1]), 'object');
     assert.notDeepStrictEqual(productDetails('alcool','mascara')[0], productDetails('alcool', 'mascara')[1]);
     assert.deepStrictEqual(productDetails('alcool', 'mascara')[0].details.productId.slice(-3), '123');
     assert.deepStrictEqual(productDetails('alcool', 'mascara')[1].details.productId.slice(-3), '123');
