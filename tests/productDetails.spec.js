02bd3e55145cad6c21b6425f20d38d5a02d70802
Nalu <aluziapantoja@gmail.com>
2021-05-06 09:03:10 -0300

    Req 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,11 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(Object.prototype.toString.call(productDetails('teste1', 'teste2')), '[object Array]');
+    assert.deepEqual(productDetails('Álcool', 'Mask').length, 2);
+    assert.deepStrictEqual(typeof productDetails('teste1', 'teste2')[0], 'object');
+    assert.notDeepStrictEqual(productDetails('teste1', 'teste2')[1], 'object');
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(productDetails('teste', 'teste').map((value) => value.details.productId.slice(-3)), ['123', '123']);
   });
 });
