653f4cd7a58ec276656e4af0cf090bd69a6faac1
Aline Hofner <alinehofner@gmail.com>
2021-05-06 13:46:04 -0300

    Altera Typeof

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,15 +34,9 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     assert.deepStrictEqual(Array.isArray(productDetails(firstProduct, secondProduct)), true);
     assert.deepStrictEqual(productDetails('firstProduct', 'secondProduct').length, 2);
     //Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/typeof
-    assert.deepStrictEqual(typeof productDetails('firstProduct', 'secondProduct')[0] && typeof productDetails('firstProduct', 'secondProduct')[1], 'object');
+    assert.strictEqual(typeof productDetails('firstProduct', 'secondProduct')[0] && typeof productDetails('firstProduct', 'secondProduct')[1], 'object');
     assert.notStrictEqual(productDetails('firstProduct', 'secondProduct')[0], productDetails('firstProduct', 'secondProduct')[1]);
     //Source: https://www.w3schools.com/jsref/jsref_endswith.asp
     assert.strictEqual(productDetails('firstProduct', 'secondProduct')[0].details.productId.endsWith('123') && productDetails('firstProduct', 'secondProduct')[1].details.productId.endsWith('123'),true);
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
