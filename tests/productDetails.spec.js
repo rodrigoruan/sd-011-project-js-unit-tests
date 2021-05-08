ad3b69c8a0ca17240cbbdfd43a7308d615fd937a
LuanAlexandre <luanrb19@gmail.com>
2021-05-08 19:34:38 -0400

    Dia 7.4 - projeto-unit-tests: requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,20 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alccol gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(productDetails('Alccol gel', 'Máscara').every(e => typeof(e) === 'object'), true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('Alccol gel', 'Máscara')[0], productDetails('Alccol gel', 'Máscara')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const products = productDetails('Alcool gel', 'Máscara');
+    const productName1 = Object.values(Object.values(Object.values(products[0]))[1])[0];
+    const productName2 = Object.values(Object.values(Object.values(products[1]))[1])[0];
+    const productsNames = [productName1, productName2];
+    assert.strictEqual(productsNames.every(e => e.endsWith('123')), true);
   });
 });
