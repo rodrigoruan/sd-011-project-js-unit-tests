1e81a518c3adef17eea8434099129c33b8741b4e
lucasmulato <cobain.lucas23@gmail.com>
2021-05-13 18:00:22 -0300

    decimo segundo commit fazendo vqv  0513180012021

--- src/vqv.js
+++ src/vqv.js
@@ -18,11 +18,12 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) return undefined;
-  
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
-trabalho na Trybe e mando muito em programação
+trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
+
+console.log(vqv('Tunico', 29));
 
 module.exports = vqv;
