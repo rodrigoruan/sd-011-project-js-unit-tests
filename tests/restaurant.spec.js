6b8452b1f17d09ed7e2353cbf402b9fc4af0978d
lucasmulato <cobain.lucas23@gmail.com>
2021-05-15 12:54:19 -0300

    vigessimo sexto commit fazendo createMenu 0515125412021

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,3 +1,4 @@
+const { create } = require('domain');
 const assert = require('assert');
 const createMenu = require('../src/restaurant');
  
@@ -48,12 +49,12 @@ const createMenu = require('../src/restaurant');
 
 describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
+    assert.deepStrictEqual(createMenu({ chave: 'valor' }), { fetchMenu: { chave: 'valor' } });
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
@@ -109,6 +110,6 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // objetoRetornado.order('coxinha');
     // objetoRetornado.pay() // Retorno: somaDosPreçosDosPedidos
     // ```
-    // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.
+    // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.    
   });
 });
