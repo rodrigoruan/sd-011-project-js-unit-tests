8c1d7d52ac852dce75b75e1cf1d50aab6bb453b5
Eicke Yumoto <eickeyumoto@gmail.com>
2021-05-18 13:43:21 -0300

    correção

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -57,9 +57,8 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
-    objetoRetornado = createMenu({ food: {}, drink: {} });
-    objetoRetornado.fetchMenu();
-    assert.deepStrictEqual(Object.keys(objetoRetornado.fetchMenu()), ['food', 'drink']);
+    const object = createMenu(objetoQualquer).fetchMenu();
+    assert.deepStrictEqual(Object.keys(object), ['food', 'drink']);
     // ```
     // TESTE 3: Verifique que o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
     // ```
