a32dfa3b88bcedd486ae68bc21dc1e8ce943c98c
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-05-18 10:51:37 -0300

    Requisitos 1 ao 3

--- src/average.js
+++ src/average.js
@@ -12,7 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = () => {
+  let counter = 0;
+  if (array.length === 0) return undefined;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') return undefined;
+    counter += array[index];
+  }
+  return Math.round(counter / array.length);
+};
+};
 // add your implementation here
 
 module.exports = average;
